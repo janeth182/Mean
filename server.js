504a36009fa5c73a96996fbd92d8950c8a86d34b
JVargas <vjanethh@gmail.com>
2017-08-25 01:18:57 -0500

    subiendo procfile 4

---
+++ server.js
@@ -0,0 +1,26 @@
+//Importando modulos///
+var express = require('express'),
+path = require('path'),
+bodyParser = require('body-parser'),
+routes = require('./server/routes/web'), //web routes
+apiRoutes = require('./server/routes/api'), //api routes
+connection = require('./server/config/db'); //mongodb connection
+
+//Creando express server
+
+var app = express();
+
+//Configuracion
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(express.static(path.join(__dirname,'app')));
+app.use(express.static('node_modules'));
+app.use('/',routes);
+app.use('/api',apiRoutes);
+
+var port = process.env.port||3000;
+
+app.listen(port, function(){
+	console.log("Server is running at: http://localhost:" + port);
+});
